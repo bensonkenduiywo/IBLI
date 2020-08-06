0cd440585e7422a8420bde44d08a0659488a2c13
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-08-06 06:06:34 -0700

    fix

--- figs/fig2.R
+++ figs/fig2.R
@@ -66,7 +66,7 @@ mort <- agrodata::data_ibli("marsabit_mortality.rds")
 mort2 <- mort[mort$type == "TLU", ]  # remote TLU for merging
 
 amort <- aggregate(mort2[,c("stock_beginning", "loss"), drop=FALSE], mort2[, c("season","year","sublocation"), drop=FALSE], mean, na.rm=TRUE)
-amort$mortality_rate <- (amort$loss / amort$stock_beginning) * 100
+amort$mortality_rate <- (amort$loss / amort$stock_beginning) # * 100
 
 
 ## Normal vs lognormal data treament 
@@ -85,62 +85,17 @@ dff <- ungroup(mutate(group_by(df1, SUBLOCATION, season), znoaa=zscore(noaa), zr
 colnames(amort)[3] <- "SUBLOCATION"
 dff <- merge(dff, amort, by=c("year", "season", "SUBLOCATION"))
 
-rrmse <- function(observed, predicted){ 
-  error <- observed - predicted
-  rmse <- sqrt(mean(error^2, na.rm=TRUE))
-  nullerror <- observed - mean(observed)
-  nullrmse <- sqrt(mean(nullerror^2, na.rm=TRUE))
-  (nullrmse - rmse) / nullrmse
-}
 
-Rsq <- function(actual, predicted) { 
-  d <- na.omit(data.frame(actual, predicted))
-  1 - (sum((d$actual-d$predicted)^2)/ sum((d$actual - mean(d$actual))^2)) 
-} 
+regfun <- function(x, y, main="", label="", ylab="") {
 
-library(segmented)
-#MQS function
-library(agro)
-mqs_test <- function(rho, df){
-  ce_base <- ce_income(df$capital, rho)
-  ce_ins  <- ce_income(df$income_ins, rho)
-  ce_ins - ce_base
-}
-
-get_benefit <- function(df, trigger, rho=1.5) {
-  #Define trigger
-  data <- na.omit(df)
-  #trigger <- quantile(data$predicted_mortality, 0.80, na.rm=T)
-  #Compute payouts
-  data$payouts <- pmax(0, data$predicted_mortality - trigger)/100 * 1000
-  #Compute Actuarily fair premiumL Premium with no mark up
-  premium <- mean(data$payouts, na.rm=T)
-  # markup 25%
-  premium <- 1.25 * premium
-  #Compute income with insurance
-  data$income_ins   <- (data$capital + data$payouts) - premium
-  #Determine insurance benefit
-  mqs_test(rho, data)
-}
-
-
-regfun <- function(x, y, triggers, main="", label="", ylab="") {
-
-y = y / 100
 
   df <- na.omit(data.frame(x=x, y=y))
   df0 <- df[df$x<0,]
   df5 <- df[df$x < -0.5,]
-  test <- data.frame(matrix(nrow=4, ncol=6+length(triggers)))
-  mqsn <- paste0("mqs", triggers)
-  nt <- length(triggers)
-  colnames(test) <- c("group", "model", "r2", "RMSE", "intercept", "slope", mqsn)
-  test$group <- main
-  test$model <- c("lm", "lm0", "lm5", "seg")
-
+ 
 	xlab <- paste0("z-score (", main, ")")
  
-  plot(y~x, pch=16, main=label, cex=0.9, cex.main=1.2, cex.axis=0.8, 
+	plot(y~x, pch=16, main=label, cex=0.9, cex.main=1.2, cex.axis=0.8, 
 	cex.lab=1.2, xlab="", ylab="", las=1)
 
 	title(xlab=xlab, line=2)
@@ -156,61 +111,22 @@ y = y / 100
   sm <- segmented(ml, seg.Z = ~x, psi=0)
   plot.segmented(sm, add=T, lwd=2, lty=1, col="red") 
   
-  df$capital <- (100 - df$y)/100 * 1000
-  e <- predict(ml)
-  df$predicted_mortality <- e
-  
-  for (i in 1:nt) test[1,i+6] <- get_benefit(df, triggers[i])
-  test[1,3:4] <- c(Rsq(df$y, e), rrmse(df$y, e))
-  test[1,5:6] <- coefficients(ml)
-  
-  e <- predict(ml2)
-  df$predicted_mortality <- NA
-  df$predicted_mortality[df$x<0] <- e
-  for (i in 1:nt) test[2,i+6] <- get_benefit(df, triggers[i])
-
-  test[2,3:4] <- c(Rsq(df0$y, e), rrmse(df0$y, e))
-  test[2,5:6] <- coefficients(ml2)
-  
-  e <- predict(ml5)
-  df$predicted_mortality <- NA
-  df$predicted_mortality[df$x < -0.5] <- e
-  for (i in 1:nt) test[3,i+6] <- get_benefit(df, triggers[i])
-  test[3,3:4] <- c(Rsq(df5$y, e), rrmse(df5$y, e))
-  test[3,5:6] <- coefficients(ml5)
-  
-  e <- predict(sm)
-  df$predicted_mortality <- e
-  for (i in 1:nt) test[4,i+6] <- get_benefit(df, triggers[i])
-  
-  test[4,3:4] <- c(Rsq(df$y, e), rrmse(df$y, e))
-  segslope <- slope(sm)$x[1]
-  bx = sm$psi[2]
-  by = predict(sm, data.frame(x=-0.92))
-  intercpt = segslope * -bx + by
-  test[4,5:6] <- c(intercpt, segslope)
-
 	if (label == "(c)") {  
 	  legend("topright", c("lm", "lm0", "lm5", "sm"), col=c("magenta", "blue", "green", "red"), lwd=c(3,3,3,2), lty=c(2,3,4,1)) #, bty="n")
-	  test
 	}
 }
 
 
-#aggregate(dff[, "mortality_rate", drop=FALSE], dff[,"SUBLOCATION", drop=FALSE], function(i) #quantile(i, c(0.75, 0.8), na.rm=T))
-
-res <- list()
 
 png("figs/fig2.png", 800, 800, pointsize = 24)
 par(mfrow=c(2, 3), mar=c(4.5, 4, 1.8, 0)) #c(bottom, left, top, right)
-res <- list()
-res$noaa <- regfun(dff$znoaa, dff$mortality_rate, trigs, "NO", "(a)", ylab="mortality rate")
-res$rain <- regfun(dff$zrain, dff$mortality_rate, trigs, "RN", "(b)")
-res$modis <- regfun(dff$zmodis, dff$mortality_rate, trigs, "MD", "(c)")
+regfun(dff$znoaa, dff$mortality_rate, "NO", "(a)", ylab="mortality rate")
+regfun(dff$zrain, dff$mortality_rate, "RN", "(b)")
+regfun(dff$zmodis, dff$mortality_rate, "MD", "(c)")
 
-res$lognoaa <- regfun(dff$zlnoaa, dff$mortality_rate, trigs, "lNO", "(d)", ylab="mortality rate")
-res$lograin <- regfun(dff$zlrain, dff$mortality_rate, trigs, "lRN", "(e)")
-res$logmodis <- regfun(dff$zlmodis, dff$mortality_rate, trigs, "lMD", "(f)")
+regfun(dff$zlnoaa, dff$mortality_rate, "lNO", "(d)", ylab="mortality rate")
+regfun(dff$zlrain, dff$mortality_rate, "lRN", "(e)")
+regfun(dff$zlmodis, dff$mortality_rate, "lMD", "(f)")
 dev.off()
 
 
--- figs/fig3.R
+++ figs/fig3.R
@@ -3,10 +3,11 @@ if (system('hostname', TRUE) == "LAPTOP-IVSPBGCA") { setwd('C:/github/IBLI')
 } else { setwd('C:/Users/camila/Google Drive/AfSF') }
 
 x <- readRDS("rmd/reg_RIB.rds")
-z <- readRDS("rmd/perfect.rds")
+
+#z <- readRDS("rmd/perfect.rds")
 
 
-tv <- "23"
+tv <- "0.18"
 tvar <- paste0("mqs", tv)
 vars <- c("group", "model", "r2", tvar)
 d <- x[, vars]
@@ -15,7 +16,6 @@ colnames(d) <- gsub(tvar, "RIB", colnames(d))
 
 png("figs/fig3.png", 800, 800, pointsize = 24)
 
-x11()
 par(mfrow=c(2, 2), mar=c(4.5, 4.2, 1.8, 1)) #c(bottom, left, top, right)
 boxplot(r2~model, data=d, ylab = expression(R^2), xlab="", las=1, main="(a)", cex.axis=.8)
 title(xlab="Model", line=2)
