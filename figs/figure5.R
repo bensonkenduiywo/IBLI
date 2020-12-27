440d61df9545168e09159ac838694c64e5c1fe40
Benson Kenduiywo <bkenduiywo@ucdavis.edu>
2020-12-27 14:18:37 +0300

    --Figure 5 edits

--- figs/figure5.R
+++ figs/figure5.R
@@ -28,13 +28,13 @@ char <- (1:6)[match(d$group, ug)]
 png("figs/figure5.png", units="in", width=6, height=6, res=600, pointsize=18)
 
 par(mar=c(4, 4, 1, 1))
-plot(d[,3:4], type="n", xlab = expression(R^2),cex=.8,
-	xlim=c(0,0.7), ylim=c(0,0.7), las=1, asp=1, xaxs="i", yaxs="i")
+plot(d[,3:4], type="n", xlab = expression(R^2),xlim=c(0,0.7), ylim=c(0,0.7),
+     las=1, asp=1, xaxs="i", yaxs="i", cex=0.8, cex.axis=0.7, cex.lab=0.7)
 #abline(lm(RIB~r2, data=d), lty=1, lwd=2)
 abline(0,1, col="light gray", lwd=4, lty=3)
 points(d[,3:4], pch=char, col=cols, cex=1.1)
-legend("topleft", legend=ug, pt.cex=1.1, pch=1:6, bty="n", cex=.8, inset=c(.02, 0))
-legend("topleft", legend=um, pch=20, pt.cex=1.5, col=pal, bty="n", cex=.8, inset=c(.2, 0))
+legend("topleft", legend=ug, pt.cex=1.1, pch=1:6, bty="n", cex=.5, inset=c(.02, 0))
+legend("topleft", legend=um, pch=20, pt.cex=1.5, col=pal, bty="n", cex=.5, inset=c(.2, 0))
 text(0.65, 0.6, "y=x", col="light gray")
 
 dev.off()
